c750035dde4f471da57a12f0808c972a5304be18
Rasmus Svala <rasmus.svala@hotmail.com>
2026-03-13 18:38:49 +0100

    second draft

--- script.js
+++ script.js
@@ -33,28 +33,3 @@ if (prefersReducedMotion) {
 
   revealElements.forEach((el) => observer.observe(el));
 }
-
-const hero = document.querySelector(".hero__image");
-const orbs = document.querySelectorAll(".orb");
-
-if (hero && orbs.length && !prefersReducedMotion) {
-  const handleMove = (event) => {
-    const bounds = hero.getBoundingClientRect();
-    const x = (event.clientX - bounds.left) / bounds.width - 0.5;
-    const y = (event.clientY - bounds.top) / bounds.height - 0.5;
-
-    orbs.forEach((orb, index) => {
-      const depth = (index + 1) * 6;
-      orb.style.setProperty("--orb-x", `${x * depth}px`);
-      orb.style.setProperty("--orb-y", `${y * depth}px`);
-    });
-  };
-
-  hero.addEventListener("mousemove", handleMove);
-  hero.addEventListener("mouseleave", () => {
-    orbs.forEach((orb) => {
-      orb.style.setProperty("--orb-x", "0px");
-      orb.style.setProperty("--orb-y", "0px");
-    });
-  });
-}
